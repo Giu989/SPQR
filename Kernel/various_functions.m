acc272245055d878ba27dd1362e2e057b9a376cd
Giu989 <giuliocrisanti100@gmail.com>
2025-07-30 21:42:26 +0100

    new function: SortVariables

--- Kernel/various_functions.m
+++ Kernel/various_functions.m
@@ -46,11 +46,10 @@ FindIrreducibleMonomials[polySystem_,vars1_,OptionsPattern[]]:=Module[
 	
 	(*numerical substitution of the parameters*)
 	params = Complement[polySystem // Variables,vars1];
-	(*paramsNsub =params->1+10^-1 (RandomSample[Range[1,10^3+(params//Length)],params//Length]//Map[Prime])(*/(RandomSample[Range[1,10+(params//Length)],params//Length]//Map[Prime])*)//Thread;*)
 	paramsNsub = Thread[params->(RandomInteger[{1,10^8+(params//Length)},params//Length]//Map[Prime])];
 	
 	If[OptionValue["Sort"],
-		Print["Warning: sorting monomials. Please use the new ordering provided as the second output of this function"];
+		Print["Warning: sorting monomials. Please use the new ordering provided as the second output of this function along with the found irreducible monomials"];
 		vars = GroebnerBasis`DistributedTermsList[polySystem // ReplaceAll[paramsNsub],vars1,MonomialOrder->OptionValue["MonomialOrder"],CoefficientDomain->RationalFunctions,Modulus->prime,Sort->True]//Last;
 		,
 		vars = vars1;
@@ -95,6 +94,24 @@ FindIrreducibleMonomials[polySystem_,vars1_,OptionsPattern[]]:=Module[
 	,
 		Return[MonomialList[Plus@@res,vars,ord]//DeleteCases[0]];
 	];
+];
+
+
+Options[SortVariables] = {"MonomialOrder" -> Lexicographic};
+SortVariables[polySystem_,vars1_,OptionsPattern[]]:=Module[
+	{
+	ord,prime,params,paramsNsub,vars
+	},
+
+	ord = OptionValue["MonomialOrder"];
+	
+	(*numerical substitution of the parameters*)
+	params = Complement[polySystem // Variables,vars1];
+	paramsNsub = Thread[params->(RandomInteger[{1,10^8+(params//Length)},params//Length]//Map[Prime])];
+	
+	vars = GroebnerBasis`DistributedTermsList[polySystem // ReplaceAll[paramsNsub],vars1,MonomialOrder->OptionValue["MonomialOrder"],CoefficientDomain->RationalFunctions,Sort->True]//Last;
+	
+	Return[vars];
 ];
 
 
